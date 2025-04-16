31c9e56e248e65d835845cc8d32795cc23d323d6
Shadydogg <95200199+Shadydogg@users.noreply.github.com>
2025-04-16 19:41:12 +0300

    Update custom.d.ts

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,5 +1,11 @@
-// src/custom.d.ts
+// ✅ Импорт PNG-изображений как строка
 declare module '*.png' {
   const value: string;
   export default value;
+}
+
+// ✅ Типизация внешнего модуля canvas-confetti
+declare module 'canvas-confetti' {
+  const confetti: any;
+  export default confetti;
 }
