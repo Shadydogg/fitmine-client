b583133299045f274e5b7f4fb8964a1c631cb403
Shadydogg <95200199+Shadydogg@users.noreply.github.com>
2025-06-06 16:50:43 +0300

    Update glb.d.ts

--- src/types/glb.d.ts
+++ src/types/glb.d.ts
@@ -1,5 +1,16 @@
 // 📄 src/types/glb.d.ts
+
+// Расширение для GLB
 declare module "*.glb" {
   const src: string;
   export default src;
+}
+
+// Расширение глобального объекта window
+export {};
+
+declare global {
+  interface Window {
+    needGoogleReauth?: boolean;
+  }
 }
