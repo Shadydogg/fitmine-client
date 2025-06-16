f25bd37098c656e25d09fb9692f2bc05b1dd625f
Shadydogg <95200199+Shadydogg@users.noreply.github.com>
2025-06-16 13:59:13 +0300

    Update PowerBank.ts

--- src/types/PowerBank.ts
+++ src/types/PowerBank.ts
@@ -1,9 +1,10 @@
 export type PowerBank = {
   id: string;
-  ep_amount: number;
-  claimed_at?: string;
   used: boolean;
   used_at?: string | null;
+  claimed_at?: string;
   powerbank_type: "basic" | "rare" | "epic" | "event";
   source?: string;
+  ep_amount: number;
+  usedToday?: boolean;
 };
